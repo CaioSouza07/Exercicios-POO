5efef9bbe05c942a7e373c6df248e595f0aff984
Caio <caiodesouza.cds@gmail.com>
2025-03-06 20:59:57 -0300

    Exercicio 1 feito!

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        //EXERCÍCIO 1
+        /*
+        System.out.println("Digite um numero: ");
+        Long numero = leitor.nextLong();
 
+        Long soma = numero + 1L;
 
+        System.out.println("A soma é: " + soma);
+        */
+
+        
     }
+
 }
